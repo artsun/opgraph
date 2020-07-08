2a0c8065c0cce8d4c1a0b70eb11ff7ce4f80c5f9
artsun <parafo@gmail.com>
2020-07-08 23:19:30 +0300

    Update server.py

--- server.py
+++ server.py
@@ -11,9 +11,6 @@ HEADERS = {
 
 app = Flask(__name__)
 
-# http://localhost:5000/?url=https://moz.com/blog/meta-data-templates-123
-# http://localhost:5000/?url=https://tproger.ru/articles/dealing-with-cyber-crime/
-
 
 @app.route('/', methods=['GET'])
 def index():
